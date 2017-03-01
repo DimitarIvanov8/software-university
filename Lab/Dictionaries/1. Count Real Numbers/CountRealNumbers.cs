2eaf75a7150d62a59c57d84d5e884d40c08da38d
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-03-02 00:15:00 +0200

    Dictionaries

---
+++ Lab/Dictionaries/1. Count Real Numbers/CountRealNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Count_Real_Numbers
+{
+    class CountRealNumbers
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+
+            var count = new SortedDictionary<double, int>();
+
+            foreach (var num in numbers)
+            {
+                if (!count.ContainsKey(num))
+                {
+                    count[num] = 0;
+                }
+                count[num]++;
+            }
+
+            foreach (var kvp in count)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
---
+++ Lab/Dictionaries/2. Odd Occurrences/OddOccurrences.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Odd_Occurrences
+{
+    class OddOccurrences
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .ToLower()
+                .Split(' ')
+                .ToArray();
+
+            var result = new Dictionary<string, int>();
+
+            foreach (var item in numbers)
+            {
+                if (!result.ContainsKey(item))
+                {
+                    result[item] = 0;
+                }
+                result[item]++;
+            }
+
+            var oddNumbers = new List<string>();
+
+            foreach (var kvp in result)
+            {
+                if (kvp.Value % 2 != 0)
+                {
+                    oddNumbers.Add(kvp.Key);
+                }
+            }
+            Console.WriteLine(String.Join(", ", oddNumbers));
+        }
+    }
+}
