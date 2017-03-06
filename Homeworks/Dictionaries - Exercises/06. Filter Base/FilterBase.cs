0f14a56fc7f4a001deb7561e0d3d3e4ee994d15e
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-03-07 01:39:08 +0200

    s

--- Homeworks/Dictionaries - Exercises/06. Filter Base/FilterBase.cs
+++ Homeworks/Dictionaries - Exercises/06. Filter Base/FilterBase.cs
@@ -10,6 +10,67 @@ namespace _06.Filter_Base
     {
         static void Main(string[] args)
         {
+            var nameAndPosition = new Dictionary<string, string>();
+            var nameAndSalary = new Dictionary<string, double>();
+            var nameAndAge = new Dictionary<string, int>();
+            int numInt = 0;
+            double numDouble = 0.0;
+
+            string line = Console.ReadLine();
+
+            while (line != "filter base")
+            {
+                var tokens = line.Split(' ');
+                var name = tokens[0];
+
+                bool intParsed = int.TryParse(tokens[2], out numInt);
+                bool doubleParse = double.TryParse(tokens[2], out numDouble);
+
+                if (intParsed == true)
+                {
+                    var age = int.Parse(tokens[2]);
+                    nameAndAge[name] = age;
+                }
+                else if (doubleParse == true)
+                {
+                    var salary = double.Parse(tokens[2]);
+                    nameAndSalary[name] = salary;
+                }
+                else
+                {
+                    var position = tokens[2];
+                    nameAndPosition[name] = position;
+                }
+
+                line = Console.ReadLine();
+            }
+
+            var filter = Console.ReadLine();
+
+            if (filter == "Position")
+            {
+                foreach (var kvp in nameAndPosition)
+                {
+                    Console.WriteLine($"Name: {kvp.Key}\nPosition: {kvp.Value}");
+                    Console.WriteLine(new string('=', 20));
+                }
+            }
+            else if (filter == "Salary")
+            {
+                foreach (var kvp in nameAndSalary)
+                {
+                    Console.WriteLine($"Name: {kvp.Key}\nSalary: {kvp.Value:F2}");
+                    Console.WriteLine(new string('=', 20));
+                }
+            }
+            else if (filter == "Age")
+            {
+                foreach (var kvp in nameAndAge)
+                {
+                    Console.WriteLine($"Name: {kvp.Key}\nAge: {kvp.Value}");
+                    Console.WriteLine(new string('=', 20));
+                }
+            }
         }
     }
 }
