1bba35762ee9b20d32538bec47508b745210806f
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-03-07 00:54:15 +0200

    d

--- Homeworks/Dictionaries - Exercises/05. User Logins/UserLogins.cs
+++ Homeworks/Dictionaries - Exercises/05. User Logins/UserLogins.cs
@@ -10,6 +10,41 @@ namespace _05.User_Logins
     {
         static void Main(string[] args)
         {
+            var nameAndPass = new Dictionary<string, string>();
+            string line = Console.ReadLine();
+
+            while (line != "login")
+            {
+                var tokens = line.Split(' ');
+                var name = tokens[0];
+                var password = tokens[2];
+
+                nameAndPass[name] = password;
+                
+                line = Console.ReadLine();
+            }
+
+            line = Console.ReadLine();
+            var unsuccessful = 0;
+
+            while (line != "end")
+            {
+                var tokens = line.Split(' ');
+                var name = tokens[0];
+                var password = tokens[2];
+         
+                if (!nameAndPass.ContainsKey(name) || nameAndPass[name] != password)
+                {
+                    Console.WriteLine($"{name}: login failed");
+                    unsuccessful++;
+                }
+                else
+                {
+                    Console.WriteLine($"{name}: logged in successfully");
+                }
+                line = Console.ReadLine();
+            }
+            Console.WriteLine($"unsuccessful login attempts: {unsuccessful}"); 
         }
     }
 }
