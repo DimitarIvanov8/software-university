c6117c9b63b24654bab8ea4ce0356af7bd08cc4a
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-03-08 20:15:05 +0200

    Advanced Collections

---
+++ Lab/Advanced Collections/1. Average Student Grades/AverageGrades.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Average_Student_Grades
+{
+    class AverageGrades
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> grades = new Dictionary<string, List<double>>();
+            var students = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < students; i++)
+            {
+                var line = Console.ReadLine().Split(' ');
+                var name = line[0];
+                double grade = double.Parse(line[1]);
+
+                if (!grades.ContainsKey(name))
+                {
+                    grades[name] = new List<double>();
+                }
+                grades[name].Add(grade);
+            }
+            foreach (var nameAndGrade in grades)
+            {
+                var average = nameAndGrade.Value.Average();
+                Console.WriteLine("{0} -> {1} (avg: {2:F2})",
+                    nameAndGrade.Key, String.Join(" ", nameAndGrade.Value.Select(
+                    p => String.Format("{0:F2}",p))), average);
+            }
+        }
+    }
+}
---
+++ Lab/Advanced Collections/2. Cities by Continent and Country/ByContinentAndCountry.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class ByContinentAndCountry
+{
+    static void Main()
+    {
+        var simpleGeography = new Dictionary<string, Dictionary<string, List<string>>>();
+        var n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            var line = Console.ReadLine().Split(' ');
+            var continent = line[0];
+            var country = line[1];
+            var city = line[2];
+
+            AddLocationss(simpleGeography, continent, country, city);
+        }
+
+        foreach (var continents in simpleGeography.Keys)
+        {
+            Console.WriteLine(continents + ":");
+            var inContinents = simpleGeography[continents];
+            foreach (var country in inContinents.Keys)
+            {
+                var towns = inContinents[country];
+                Console.WriteLine("  {0} -> {1}", country,
+                    String.Join(", ",towns));
+            }
+        }
+    }
+
+    private static void AddLocationss(Dictionary<string, Dictionary<string, List<string>>> 
+        simpleGeography, string continent, string country, string city)
+    {
+        if (!simpleGeography.ContainsKey(continent))
+        {
+            simpleGeography[continent] = new Dictionary<string, List<string>>();
+        }
+        if (!simpleGeography[continent].ContainsKey(country))
+        {
+            simpleGeography[continent][country] = new List<string>(); 
+        }
+        simpleGeography[continent][country].Add(city);
+    }
+}
+
