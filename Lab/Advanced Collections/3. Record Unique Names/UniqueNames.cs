76f6a83afda094f66e25197ea6ddc0fdf3674ae1
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-03-09 18:55:09 +0200

    Adding exercises

---
+++ Lab/Advanced Collections/3. Record Unique Names/UniqueNames.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Record_Unique_Names
+{
+    class UniqueNames
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+            Console.WriteLine(String.Join(Environment.NewLine,names));
+        }
+    }
+}
---
+++ Lab/Advanced Collections/4. Group Continents, Countries and Cities/CountryCityAndContinent.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Group_Continents__Countries_and_Cities
+{
+    class CountryCityAndContinent
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, SortedDictionary<string, SortedSet<string>>> location =
+                new SortedDictionary<string, SortedDictionary<string, SortedSet<string>>>();
+
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Split(' ');
+                var continent = line[0];
+                var country = line[1];
+                var city = line[2];
+
+                AddLocations(location, continent, country, city);
+            }
+
+            foreach (var continentsCountries in location)
+            {
+                var continentName = continentsCountries.Key;
+                var countries = continentsCountries.Value;
+
+                Console.WriteLine($"{continentName}:");
+                foreach (var countryCities in countries)
+                {
+                    var countryName = countryCities.Key;
+                    var cities = countryCities.Value;
+                    Console.WriteLine("  {0} -> {1}",
+                        countryName, String.Join(", ", cities));
+                }
+            }
+        }
+
+        private static void AddLocations(SortedDictionary<string, SortedDictionary<string, SortedSet<string>>> 
+            location, string continent, string country, string city)
+        {
+            if (!location.ContainsKey(continent))
+            {
+                location[continent] = new SortedDictionary<string, SortedSet<string>>();
+            }
+            if (!location[continent].ContainsKey(country))
+            {
+                location[continent][country] = new SortedSet<string>();
+            }
+            location[continent][country].Add(city);
+        }
+    }
+}
