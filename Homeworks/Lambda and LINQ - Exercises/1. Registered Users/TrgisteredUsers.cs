526dcf8e702ab90eee7f1c9e1eb40845221d98b2
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-03-12 17:39:50 +0200

    Lambda and LINQ - Exercises

---
+++ Homeworks/Lambda and LINQ - Exercises/1. Registered Users/TrgisteredUsers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+
+namespace _1.Registered_Users
+{
+    class TrgisteredUsers
+    {
+        static void Main(string[] args)
+        {
+            var nameAndDate = Console.ReadLine()
+                .Split(new[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            var registry = new Dictionary<string, DateTime>();
+
+            while (nameAndDate[0] != "end")
+            {
+                var name = nameAndDate[0];
+                var date = DateTime.ParseExact(nameAndDate[1], "dd/MM/yyyy",CultureInfo.InvariantCulture);
+                registry.Add(name, date);
+
+                nameAndDate = Console.ReadLine()
+                .Split(new[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            }
+
+            var resultRegistry = registry
+                .Reverse()
+                .OrderBy(x => x.Value)
+                .Take(5)
+                .OrderByDescending(x => x.Value)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var item in resultRegistry)
+            {
+                Console.WriteLine($"{item.Key}");
+            }
+        }
+    }
+}
---
+++ Homeworks/Lambda and LINQ - Exercises/2. Default Values/DefaultValues.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Default_Values
+{
+    class DefaultValues
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine()
+                .Split(new[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var registry = new Dictionary<string, string>();
+
+            while (line[0] != "end")
+            {
+                var inputKey = line[0];
+                var inputValue = line[1];
+                registry[inputKey] = inputValue;
+
+                line = Console.ReadLine()
+                .Split(new[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            }
+            var defaultValue = Console.ReadLine();
+
+            var sortedRegistry = registry //keys that have values
+                .Where(x => x.Value != "null")
+                .OrderByDescending(x => x.Value.Length)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            registry = registry //keys without values
+                .Where(x => x.Value == "null")
+                .ToDictionary(x => x.Key, x => defaultValue);
+            
+            foreach (var item in registry)
+            {
+                if (!sortedRegistry.ContainsKey(item.Key))
+                {
+                    sortedRegistry[item.Key] = defaultValue;
+                }
+            }
+
+            foreach (var item in sortedRegistry)
+            {
+                Console.WriteLine($"{item.Key} <-> {item.Value}");;
+            }
+        }
+    }
+}
