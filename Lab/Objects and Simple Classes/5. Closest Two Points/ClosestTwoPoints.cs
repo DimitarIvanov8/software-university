6838e7cbe8b59cd1545dbb4f051e04543a27e816
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-03-20 19:50:30 +0200

    problems 5 and 6

---
+++ Lab/Objects and Simple Classes/5. Closest Two Points/ClosestTwoPoints.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Closest_Two_Points
+{
+    class ClosestTwoPoints
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var points = new List<Point>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentPoint = ReadPoint();
+                points.Add(currentPoint);
+            }
+
+            var minimumDistace = double.MaxValue;
+            Point firstPointResult = null;
+            Point secondPointResult = null;
+
+            for (int first = 0; first < points.Count; first++)
+            {
+                for (int second = first + 1; second < points.Count; second++)
+                {
+                    var firstPoint = points[first];
+                    var secondPoint = points[second];
+                    var distance = Distance(firstPoint, secondPoint);
+
+                    if (distance < minimumDistace)
+                    {
+                        minimumDistace = distance;
+                        firstPointResult = firstPoint;
+                        secondPointResult = secondPoint;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{minimumDistace:F3}");
+            Console.WriteLine(firstPointResult.Print());
+            Console.WriteLine(secondPointResult.Print());
+        }
+
+        public static double Distance(Point first, Point second)
+        {
+            var squareX = Math.Pow(first.X - second.X, 2);
+            var squareY = Math.Pow(first.Y - second.Y, 2);
+            var result = Math.Sqrt(squareX + squareY);
+            return result;
+        }
+
+        public static Point ReadPoint()
+        {
+            var pointParts = Console.ReadLine()
+                .Split(' ');
+
+            return new Point
+            {
+                X = double.Parse(pointParts[0]),
+                Y = double.Parse(pointParts[1])
+            };
+        }
+    }
+}
---
+++ Lab/Objects and Simple Classes/6. Rectangle Position/RectanglePosition.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Rectangle_Position
+{
+    class RectanglePosition
+    {
+        static void Main(string[] args)
+        {
+            var firstRect = ReadRectangle();
+            var secondRect = ReadRectangle();
+
+            var result = firstRect.IsInside(secondRect);
+
+            var printResult = result 
+                ? "Inside" 
+                : "Not inside";
+
+            Console.WriteLine(printResult);
+        }
+
+        public static Rectangle ReadRectangle()
+        {
+            var rectangleParts = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            return new Rectangle
+            {
+                Left = rectangleParts[0],
+                Top = rectangleParts[1],
+                Width = rectangleParts[2],
+                Height = rectangleParts[3]
+            };
+        }
+    }
+}
