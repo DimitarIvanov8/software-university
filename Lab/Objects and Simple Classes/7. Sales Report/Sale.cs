493df33a47c88f1258fdaeefc43350db0c333f13
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-03-21 00:00:38 +0200

    SalesReport

---
+++ Lab/Objects and Simple Classes/7. Sales Report/Sale.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Sales_Report
+{
+    class Sale
+    {
+        public string Town { get; set; }
+
+        public string Product { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        public static Sale Parse(string currentSaleAsString)
+        {
+            var sale = currentSaleAsString
+                .Split(' ');
+
+            return new Sale
+            {
+                Town = sale[0],
+                Product = sale[1],
+                Price = decimal.Parse(sale[2]),
+                Quantity = decimal.Parse(sale[3])
+            };
+        }
+    }
+}
---
+++ Lab/Objects and Simple Classes/7. Sales Report/SalesReport.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Sales_Report
+{
+    class SalesReport
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            SortedDictionary<string, decimal> salesByTown = new SortedDictionary<string, decimal>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentSaleAsString = Console.ReadLine();
+                var currentSale = Sale.Parse(currentSaleAsString);
+
+                if (!salesByTown.ContainsKey(currentSale.Town))
+                {
+                    salesByTown[currentSale.Town] = 0;
+                }
+
+                salesByTown[currentSale.Town] += currentSale.Quantity * currentSale.Price;
+            }
+
+            foreach (var townAndProfit in salesByTown)
+            {
+                Console.WriteLine($"{townAndProfit.Key} -> {townAndProfit.Value:F2}");
+            }
+        }       
+    }
+}
