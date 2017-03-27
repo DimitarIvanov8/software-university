c75fba0eb9b69025c79053753e57d12a250c7184
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-03-27 21:10:51 +0300

    FolderSize.cs

---
+++ Lab/Files, Directories and Exceptions/5. Folder Size/FolderSize.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _5.Folder_Size
+{
+    class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string[] fileNames = Directory.GetFiles("../../TestFolder");
+
+            double sumSize = 0;
+
+            foreach (var file in fileNames)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                sumSize += fileInfo.Length;
+            }
+
+            File.WriteAllText("../../output.txt", (sumSize / 1024 / 1024) + " MB");
+        }
+    }
+}
