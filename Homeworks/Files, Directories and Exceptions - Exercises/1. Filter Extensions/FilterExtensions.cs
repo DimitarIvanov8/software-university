b74cd9d0ed8bb563f2f3ddcfff159bbaa8a285b2
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-03-30 21:24:59 +0300

    Files, Directories and Exceptions - Exercises

---
+++ Homeworks/Files, Directories and Exceptions - Exercises/1. Filter Extensions/FilterExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _1.Filter_Extensions
+{
+    class FilterExtensions
+    {
+        static void Main(string[] args)
+        {
+            string[] fileNames = Directory.GetFiles("input");
+            var fileExtension = Console.ReadLine();
+            var files = new List<string>();
+
+            foreach (var file in fileNames)
+            {
+                if (file.Contains(fileExtension))
+                {
+                    files.Add(file);
+                }
+            }
+            Console.WriteLine(String.Join(Environment.NewLine,files));
+        }
+    }
+}
