e0bdb3db4f82b349f2393cf69d994c3ad99f682e
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-04-05 21:38:47 +0300

    SentenceSplit.cs

---
+++ Homeworks/Strings and Text Processing - Exercises/4. Sentence Split/SentenceSplit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Sentence_Split
+{
+    class SentenceSplit
+    {
+        static void Main(string[] args)
+        {
+            var sentence = Console.ReadLine();
+            var delimeter = Console.ReadLine();
+            var result = sentence.Split(new string[] { delimeter }, StringSplitOptions.None).ToList();
+
+            Console.WriteLine($"[{string.Join(", ", result)}]");
+        }
+    }
+}
