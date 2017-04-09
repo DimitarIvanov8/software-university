887a645c3dc35201fa395d1e4b3e52789d2d40a1
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-04-09 21:04:52 +0300

    Strings and Text Processing - More Exercises

---
+++ Homeworks/Strings and Text Processing - More Exercises/1. Serialize String/SerializeString.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Serialize_String
+{
+    class SerializeString
+    {
+        static void Main(string[] args)
+        {
+            var index = -2;
+            int count = 0;
+            string input = Console.ReadLine();
+            string output = "";
+            var Array = input.ToCharArray();
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (!output.Contains(Array[i]))
+                {
+                    output += Array[i];
+                    output += ":";
+
+                    for (int j = 0; j < Array.Length; j++)
+                    {
+                        count = input.IndexOf(Array[i], j);
+                        if (count != -1 && count != index)
+                        {
+                            output += count;
+                            output += "/";
+                        }
+                        index = count;
+                    }
+                    index = 0;
+                    output = output.Remove(output.Length - 1);
+                    output += Environment.NewLine;
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Homeworks/Strings and Text Processing - More Exercises/2. Stateless/Stateless.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Stateless
+{
+    class Stateless
+    {
+        static void Main(string[] args)
+        {
+            var lineFirst = Console.ReadLine();
+            string lineSecond = Console.ReadLine();
+            while (lineFirst != "collapse" && lineSecond != "collapse")
+            {
+                while (lineSecond.Length > 0)
+                {
+                    lineFirst = lineFirst.Replace(lineSecond, "").Trim();
+                    if (lineSecond.Length == 1) break;
+                    lineSecond = lineSecond.Remove(0, 1).Remove(lineSecond.Length - 2, 1).Trim();
+
+                }
+                if (lineFirst.Length == 0)
+                {
+                    Console.WriteLine("(void)");
+                }
+                else
+                {
+                    Console.WriteLine(lineFirst);
+                }
+
+                lineFirst = Console.ReadLine();
+                lineSecond = Console.ReadLine();
+            }
+        }
+    }
+}
