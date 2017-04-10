1eeb38927249706b99c0e249608ce9127463d5d4
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-04-10 21:53:25 +0300

    Regular Expressions (RegEx)/Replace a tag

---
+++ Lab/Regular Expressions (RegEx)/Replace a tag/ReplaceATag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replace_a_tag
+{
+    class ReplaceATag
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            while (text != "end")
+            {
+                string pattern = @"<a.*?href.*?=(.*)>(.*?)<\/a>";
+                string replace = @"[URL href=$1]$2[/URL]";
+
+                string replaced = Regex.Replace(text, pattern, replace);
+
+                Console.WriteLine(replaced);
+
+                text = Console.ReadLine();
+            }
+        }
+    }
+}
