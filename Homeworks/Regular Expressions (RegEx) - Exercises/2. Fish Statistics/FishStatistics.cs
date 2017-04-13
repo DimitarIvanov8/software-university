ab78ab81dfe6f3339f2817d542883d587a8d6001
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-04-13 21:20:40 +0300

    Regular Expressions (RegEx) - Exercises Template

---
+++ Homeworks/Regular Expressions (RegEx) - Exercises/2. Fish Statistics/FishStatistics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Fish_Statistics
+{
+    class FishStatistics
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
