96afcceb8dbe662d4a2114bde650b32ea1b59298
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-04-16 13:06:26 +0300

    Exercises 1 and 2 from a practice exam

--- Programming Fundamentals - Exam preparation/1. Sino The Walker/SinoTheWalker.cs
+++ Programming Fundamentals - Exam preparation/1. Sino The Walker/SinoTheWalker.cs
@@ -10,6 +10,22 @@ namespace _1.Sino_The_Walker
     {
         static void Main(string[] args)
         {
+            TimeSpan span = TimeSpan.Parse(Console.ReadLine());
+            
+            //alternatively we can take the initial input like this:
+            //var timeFormat = @"hh\:mm\:ss";
+            //var span = TimeSpan.ParseExact(Console.Readline(), timeFormat, CultureInfo.InvariantCulture);
+
+            var stepsCount = int.Parse(Console.ReadLine());
+            var stepsPerSecond = int.Parse(Console.ReadLine());
+
+            int secondsPerDay = 60 * 60 * 24;
+            var totalSecondsNeeded = (int)(((double)stepsCount * stepsPerSecond) % secondsPerDay);
+
+            TimeSpan traveledInSeconds = new TimeSpan(0, 0, totalSecondsNeeded);
+            var timeArrival = span.Add(traveledInSeconds);
+
+            Console.WriteLine("Time Arrival: {0:hh\\:mm\\:ss}", timeArrival);
         }
     }
 }
---
+++ Programming Fundamentals - Exam preparation/2. SoftUni Karaoke/SoftUniKaraoke.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.SoftUni_Karaoke
+{
+    class SoftUniKaraoke
+    {
+        static void Main(string[] args)
+        {
+            var peopleSongsAndAwards = new Dictionary<string, List<string>>();
+            var participants = Console.ReadLine().Split(new[] { ' ', ',' },StringSplitOptions.RemoveEmptyEntries).ToList();
+            var songs = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None);
+
+            var line = Console.ReadLine();
+            while (line != "dawn")
+            {
+                string[] tokens = line.Split(new[] { ',' });
+                var currentName = tokens[0].Trim();
+                var currentSong = tokens[1].Trim();
+                var currentAward = tokens[2].Trim();
+
+                if (participants.Contains(currentName) && songs.Contains(currentSong)) //need fix
+                {
+                    if (!peopleSongsAndAwards.ContainsKey(currentName))
+                    {
+                        peopleSongsAndAwards[currentName] = new List<string>();
+                    }
+
+                    if (!peopleSongsAndAwards[currentName].Contains(currentAward))
+                    {
+                        peopleSongsAndAwards[currentName].Add(currentAward);
+                    }
+                }
+
+                line = Console.ReadLine();
+            }
+
+            if (peopleSongsAndAwards.Values.Count == 0)
+            {
+                Console.WriteLine("No awards");
+                return;
+            }
+
+            foreach (var item in peopleSongsAndAwards
+                .OrderByDescending(x => x.Value.Count)
+                .ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value.Count} awards");
+                item.Value.Sort();
+
+                foreach (var award in item.Value)
+                {
+                    Console.WriteLine($"--{award}");
+                }
+            }
+        }
+    }
+}
