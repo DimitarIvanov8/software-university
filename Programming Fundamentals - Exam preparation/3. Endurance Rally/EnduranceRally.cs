8b912365d49f8c4704340c7ace979cbcf809308e
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-04-17 18:18:23 +0300

    Exercises 3 and 4 of Exam preparation 1

---
+++ Programming Fundamentals - Exam preparation/3. Endurance Rally/EnduranceRally.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.Endurance_Rally
+{
+    class Racer
+    {
+        public string Name { get; set; }
+        public decimal Fuel { get; set; }
+        public int ZoneReached { get; set; }
+        public bool isTankEmptry { get; set; }
+    }
+
+    class EnduranceRally
+    {
+        private static decimal GetFuelAmount(char firstLetter)
+        {
+            var fuelLevel = (int)firstLetter;
+            return fuelLevel;
+        }
+
+        static void Main(string[] args)
+        {
+            List<Racer> racers = new List<Racer>();
+
+            var drivers = Console.ReadLine().Split(' ').ToList();
+            var zones = Console.ReadLine().Split(' ').Select(decimal.Parse).ToList();
+            var checkpointIndexes = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            for (int i = 0; i < drivers.Count; i++)
+            {
+                var currentName = drivers[i];
+
+                Racer currentRacer = new Racer();
+                currentRacer.Name = currentName;
+                currentRacer.Fuel = GetFuelAmount(currentName[0]); //Finds the fuel according to first Letter of the name using ASCII table
+                currentRacer.isTankEmptry = false;
+
+                racers.Add(currentRacer);
+            }
+
+            foreach (var racer in racers)
+            {
+                for (int i = 0; i < zones.Count; i++)
+                {
+                    if (checkpointIndexes.Contains(i)) //Checkpoint (adding fuel)
+                    {
+                        racer.Fuel += zones[i];
+                        racer.ZoneReached = i;
+                    }
+                    else //Non checkpoint
+                    {
+                        racer.Fuel -= zones[i];
+
+                        if (racer.Fuel <= 0)
+                        {
+                            racer.isTankEmptry = true;
+                            racer.ZoneReached = i;
+                            racer.Fuel = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            foreach (var racer in racers)
+            {
+                Console.Write($"{racer.Name} - ");
+                if (racer.isTankEmptry == true)
+                {
+                    Console.WriteLine($"reached {racer.ZoneReached}");
+                }
+                else
+                {
+                    Console.WriteLine($"fuel left {racer.Fuel:F2}");
+                }
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals - Exam preparation/4. Winning Ticket/WinningTicket.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _4.Winning_Ticket
+{
+    class WinningTicket
+    {
+        static void Main(string[] args)
+        {
+            Regex ticketPattern = new Regex(@"([$#@^])\1{5,}");
+            var tickets = Console.ReadLine()
+                .Split(',')
+                .Select(x => x.Trim())
+                .ToArray();
+
+            foreach (var ticket in tickets)
+            {
+                if (ticket.Length != 20)
+                {
+                    Console.WriteLine("invalid ticket");
+                    continue;
+                }
+
+                var leftHalf = ticket.Substring(0, ticket.Length / 2);
+                var rightHalf = ticket.Substring(ticket.Length / 2);
+
+                var leftMatch = ticketPattern.Match(leftHalf);
+                var rightMatch = ticketPattern.Match(rightHalf);
+
+                if (leftMatch.Success && rightMatch.Success)
+                {
+                    var winningSymbol = leftMatch.Value[0];
+                    var shorterMatch = Math.Min(leftMatch.Length, rightMatch.Length);
+
+                    var jackpot = string.Empty;
+
+                    if (shorterMatch == 10)
+                    {
+                        jackpot = " Jackpot!";
+                    }
+                    Console.WriteLine($"ticket \"{ticket}\" - {shorterMatch}{winningSymbol}{jackpot}");
+                }
+                else
+                {
+                    Console.WriteLine($"ticket \"{ticket}\" - no match");
+                }
+            }
+        }
+    }
+}
