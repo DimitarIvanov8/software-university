a44beeac1066f95520608d183142f434ca1a24e9
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-04-23 13:46:01 +0300

    Debugging - Melrah Shake for exam Preparation

---
+++ Programming Fundamentals - Exam preparation/Debugging - Melrah Shake/MelrahShake.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Debugging___Melrah_Shake
+{
+    class MelrahShake
+    {
+        static void Main(string[] args)
+        {
+            var inputString = Console.ReadLine();
+            var inputPattern = Console.ReadLine();
+            Regex regex = new Regex(Regex.Escape(inputPattern));
+            var matches = regex.Matches(inputString);
+
+            while (matches.Count >= 2 && inputPattern.Length > 0)
+            {
+                var startIndex = inputString.IndexOf(inputPattern);
+                var lastIndex = inputString.LastIndexOf(inputPattern);
+
+                inputString = inputString.Remove(lastIndex, inputPattern.Length);
+                inputString = inputString.Remove(startIndex, inputPattern.Length);
+                Console.WriteLine("Shaked it.");
+
+                inputPattern = inputPattern.Remove((inputPattern.Length / 2), 1);
+
+                regex = new Regex(Regex.Escape(inputPattern));
+                matches = regex.Matches(inputString);
+            }
+            Console.WriteLine("No shake.");
+            Console.WriteLine(inputString);
+        }
+    }
+}
