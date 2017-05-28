e28220c1e685e42f10e081aa7257cf6638d80de6
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-05-28 22:57:19 +0300

    Exercises 4 and 5 of Stacks and Queues

---
+++ Homeworks/Stacks and Queues - Excercise/4. Basic Queue Operations/BasicQueOperations.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Basic_Queue_Operations
+{
+    class BasicQueOperations
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var queue = new Queue<int>();
+
+            var numsToEnqueue = input[0];
+            var numsToDequeue = input[1];
+            var isNumPresent = input[2];
+
+            var currentNums = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            if (currentNums.Length != numsToEnqueue)
+            {
+                return;
+            }
+
+            for (int i = 0; i < currentNums.Length; i++)
+            {
+                queue.Enqueue(currentNums[i]);
+            }
+
+            for (int i = 0; i < numsToDequeue; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(isNumPresent))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                if (queue.Count == 0)
+                {
+                    Console.WriteLine(0);
+                }
+                else
+                {
+                    Console.WriteLine(queue.Min());
+                }
+            }
+        }
+    }
+}
---
+++ Homeworks/Stacks and Queues - Excercise/5. Sequence With Queue/SequenceWithQueue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Sequence_With_Queue
+{
+    class SequenceWithQueue
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            Queue<long> queue = new Queue<long>();
+            queue.Enqueue(n);
+
+            for (int i = 0; i < 50; i++)
+            {
+                var currentElement = queue.Dequeue();
+                Console.Write(currentElement + " ");
+                queue.Enqueue(currentElement + 1);
+                queue.Enqueue(2 * currentElement + 1);
+                queue.Enqueue(currentElement + 2);
+            }
+            Console.WriteLine();
+        }
+    }
+}
