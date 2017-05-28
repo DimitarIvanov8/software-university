d301bbff751ebd27fb4904b4ba96ce2fe91c13af
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-05-28 22:56:47 +0300

    Exercise 2 and 3

---
+++ Homeworks/Stacks and Queues - Excercise/2. Basic Stack Operations/StackOperations.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Basic_Stack_Operations
+{
+    class StackOperations
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(new[] { ' '}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var stack = new Stack<int>();
+
+            var numsToPush = input[0];
+            var numsToPop = input[1];
+            var isNumPresent = input[2];
+
+            var currentNums = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            if (currentNums.Length != numsToPush)
+            {
+                return;
+            }
+
+            for (int i = 0; i < currentNums.Length; i++)
+            {
+                stack.Push(currentNums[i]);
+            }
+
+            for (int i = 0; i < numsToPop; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Contains(isNumPresent))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                if (stack.Count == 0)
+                {
+                    Console.WriteLine(0);
+                }
+                else
+                {
+                    Console.WriteLine(stack.Min());
+                }
+            }
+        }
+    }
+}
---
+++ Homeworks/Stacks and Queues - Excercise/3. Maximum Element/MaxElement.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Maximum_Element
+{
+    class MaxElement
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var stack = new Stack<long>();
+            var stackMaxNumbers = new Stack<long>();
+
+            var maxNumInStack = long.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentNumbers = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+                var currentNum = 0l;
+
+                if (currentNumbers.Length == 1)
+                {
+                    currentNum = currentNumbers[0];
+                    if (currentNum == 2)
+                    {
+                        if (stack.Pop() == maxNumInStack)
+                        {
+                            stackMaxNumbers.Pop();
+
+                            if (stackMaxNumbers.Count() != 0)
+                            {
+                                maxNumInStack = stackMaxNumbers.Peek();
+                            }
+                            else
+                            {
+                                maxNumInStack = long.MinValue;
+                            }
+                        }                       
+                    }
+                    else if (currentNum == 3)
+                    {
+                        Console.WriteLine(maxNumInStack);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    currentNum = currentNumbers[1];
+                    if (currentNum >= 1 && currentNumbers[0] == 1)
+                    {
+                        stack.Push(currentNum);
+
+                        if (currentNum > maxNumInStack) //max check
+                        {
+                            maxNumInStack = currentNum;
+                            stackMaxNumbers.Push(maxNumInStack);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
