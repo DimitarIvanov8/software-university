23a0c98f119d2f68790b78045dcc285ebde07bc7
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-05-30 20:31:55 +0300

    Exercises 8 and 9 from StackFibonacci.cs

---
+++ Homeworks/Stacks and Queues - Excercise/9. Stack Fibonacci/StackFibonacci.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Stack_Fibonacci
+{
+    class StackFibonacci
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
