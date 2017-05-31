22688b08796f05f8bc4a22df32334e7a82a376da
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-05-31 23:52:46 +0300

    Sets and Dictionaries - Lab

---
+++ Lab/Sets and Dictionaries - Lab/2. SoftUniParty/SoftUniParty.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.SoftUniParty
+{
+    class SoftUniParty
+    {
+        static void Main(string[] args)
+        {
+            var reserverations = new SortedSet<string>();
+            var input = Console.ReadLine();
+
+            while (input != "PARTY")
+            {
+                reserverations.Add(input);
+                input = Console.ReadLine();
+            } 
+
+            while (input != "END")
+            {
+                reserverations.Remove(input);
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(reserverations.Count);
+            foreach (var reservation in reserverations)
+            {
+                Console.WriteLine(reservation);
+            }
+        }
+    }
+}
