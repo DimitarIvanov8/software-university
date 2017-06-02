eedceb04913e6a90c0f922fb96e8ced9783d004b
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-06-02 23:35:52 +0300

    Sets and Dictionaries - Exercise/1. Unique Usernames/1. Unique Usernames.csproj

---
+++ Homeworks/Sets and Dictionaries - Exercise/1. Unique Usernames/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a simple program that reads from the console a sequence of usernames
+            // and keeps a collection with only the unique ones. Print the collection on the console.
+
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            HashSet<string> mySet = new HashSet<string>();
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string input = Console.ReadLine();
+                mySet.Add(input);
+            }
+
+            foreach (var element in mySet)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
