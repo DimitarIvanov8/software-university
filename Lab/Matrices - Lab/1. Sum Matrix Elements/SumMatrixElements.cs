ec0b83438be472de9c08fa0d886793426f5f99e2
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-06-04 20:55:55 +0300

    Matrices - Lab.sln

---
+++ Lab/Matrices - Lab/1. Sum Matrix Elements/SumMatrixElements.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Sum_Matrix_Elements
+{
+    class SumMatrixElements
+    {
+        static void Main(string[] args)
+        {
+            var matrixSizes = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None).Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[matrixSizes[0], matrixSizes[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var inputRow = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None).Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputRow[col];
+                }
+            }
+
+            int maxSum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    maxSum += matrix[row, col];
+                }
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(maxSum);
+        }
+    }
+}
---
+++ Lab/Matrices - Lab/2. Maximum sum of 2x2 submatrix/MaxSumOfSubmatrix.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Maximum_sum_of_2x2_submatrix
+{
+    class MaxSumOfSubmatrix
+    {
+        static void Main(string[] args)
+        {
+            var matrixSizes = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None).ToArray();
+
+            int[][] matrix = new int[int.Parse(matrixSizes[0])][];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                matrix[row] = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None).Select(int.Parse).ToArray();
+            }
+
+            var maxSquareRow = 0;
+            var maxSqaureCol = 0;
+            var maxSum = int.MinValue;
+
+            for (int row = 0; row < matrix.Length - 1; row++)
+            {
+                for (int col = 0; col < matrix[row].Length - 1; col++)
+                {
+                    var currentSum = matrix[row][col] + matrix[row][col + 1] + 
+                        matrix[row + 1][col] + matrix[row + 1][col + 1];
+
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                        maxSquareRow = row;
+                        maxSqaureCol = col;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{matrix[maxSquareRow][maxSqaureCol]} {matrix[maxSquareRow][maxSqaureCol + 1]}\n{matrix[maxSquareRow + 1][maxSqaureCol]} {matrix[maxSquareRow + 1][maxSqaureCol + 1]}");
+            Console.WriteLine(maxSum);
+        }
+    }
+}
