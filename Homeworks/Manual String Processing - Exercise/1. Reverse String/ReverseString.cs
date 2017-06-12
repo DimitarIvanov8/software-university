4f50531391169364e6a24a1f8f373d6c6ac4d6c6
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-06-12 18:28:21 +0300

    Manual String Processing - Exercise

---
+++ Homeworks/Manual String Processing - Exercise/1. Reverse String/ReverseString.cs
@@ -0,0 +1,28 @@
+﻿/*
+Write a program that reads a string from the console, 
+reverses it and prints the result back at the console.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Reverse_String
+{
+    class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            var inputText = Console.ReadLine();
+            var reversedText = new StringBuilder();
+
+            for (int i = inputText.Length - 1; i >= 0; i--)
+            {
+                reversedText.Append(inputText[i]);
+            }
+            Console.WriteLine(reversedText);
+        }
+    }
+}
