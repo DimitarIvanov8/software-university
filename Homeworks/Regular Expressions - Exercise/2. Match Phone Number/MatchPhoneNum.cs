bf71fcb00a11b557ba3f4772c8b9c95b4256fed5
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-06-18 19:29:41 +0300

    Homeworks/Regular Expressions - Exercise 2

---
+++ Homeworks/Regular Expressions - Exercise/2. Match Phone Number/MatchPhoneNum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace _2.Match_Phone_Number
+{
+    class MatchPhoneNum
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Regex regex = new Regex(@"^\+359-\d-\d{3}-\d{4}$");
+            Regex regexTwo = new Regex(@"^\+359 \d \d{3} \d{4}$");
+
+            while (text != "end")
+            {
+                if (regex.IsMatch(text) || regexTwo.IsMatch(text))
+                {
+                    Console.WriteLine(text);
+                }
+
+                text = Console.ReadLine();
+            }
+        }
+    }
+}
