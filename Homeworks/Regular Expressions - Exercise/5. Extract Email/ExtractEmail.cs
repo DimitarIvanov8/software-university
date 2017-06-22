20a1985eafdb2eec6e62ae26e3986248ebfc474d
Dimitar Ivanov <dimitar.ivanov300@gmail.com>
2017-06-22 18:36:39 +0200

    ExtractEmail.cs

---
+++ Homeworks/Regular Expressions - Exercise/5. Extract Email/ExtractEmail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace _5.Extract_Email
+{
+    class ExtractEmail
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(@"((?<=\s)[a-zA-Z0-9]+([-.]\w*)*@[a-zA-Z]+?([.-][a-zA-Z]*)*(\.[a-z]{2,}))");
+
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match item in matches)
+            {
+                Console.WriteLine(item.Value);
+            }
+        }
+    }
+}
