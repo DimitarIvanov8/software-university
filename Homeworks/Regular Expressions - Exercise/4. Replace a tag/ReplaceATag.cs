d06059b3277cf98e1403ce64febbc823b5db8c63
Dimitar Ivanov <dimitar.ivanov300@gmail.com>
2017-06-22 18:36:20 +0200

    ReplaceATag.cs

---
+++ Homeworks/Regular Expressions - Exercise/4. Replace a tag/ReplaceATag.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace _4.Replace_a_tag
+{
+    class ReplaceATag
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            while (true)
+            {
+                if (input == "end")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(Regex.Replace(input, @"<a.*?href.*?=(.*?)>(.*?)<\/a>", @"[URL href=$1]$2[/URL]"));
+
+                    input = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
