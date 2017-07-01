e11ae58703f1069b49a3bc082b2a40e27a87745d
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-07-02 00:09:24 +0300

    Define a Class - Exercise

---
+++ C#_OOP_Basics/Defining_Classes/Exercise/Defining Classes - Exercise/Define a Class Person/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace Define_a_Class_Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+        }
+    }
+}
---
+++ C#_OOP_Basics/Defining_Classes/Exercise/Defining Classes - Exercise/Define a Class Person/StartUp.cs
@@ -0,0 +1,44 @@
+﻿/*
+Create class Person with fields name and age.Create a class Family. The class should have list 
+of people, method for adding members(void AddMember(Person member)) and a method returning the 
+oldest family member(Person GetOldestMember()). Write a program that reads name and age for 
+N people and add them to the family.Then print the name and age of the oldest member.
+
+Example input:
+5
+Steve 10
+Christopher 15
+Annie 4
+Ivan 35
+Maria 34
+
+output:
+Ivan 35
+*/
+
+using System;
+using System.Reflection;
+
+namespace Define_a_Class_Person
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numberOfPeople = int.Parse(Console.ReadLine());
+            var family = new Family();
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                var personInfo = Console.ReadLine().Split(' ');
+                var personName = personInfo[0];
+                var personAge = int.Parse(personInfo[1]);
+
+                Person currentPerson = new Person(personAge, personName);
+                family.AddMember(currentPerson);
+            }
+
+            var oldestPerson = family.GetOldestMember();
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+        }
+    }
+}
