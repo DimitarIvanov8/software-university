a6dd575dd204b9dc7532dd133c9b6f2ad7cd7b1f
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-07-03 19:58:49 +0300

    Data Modifier exercise

---
+++ C#_OOP_Basics/Defining_Classes/Exercise/Defining Classes - Exercise/Date Modifier/DateModifier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Date_Modifier
+{
+    class DateModifier
+    {
+        public static int DifferenceBetweenTwoDates(string date1, string date2)
+        {
+            DateTime firstDate = DateTime.ParseExact(date1, "yyyy MM dd", CultureInfo.InvariantCulture);
+            DateTime secondDate = DateTime.ParseExact(date2, "yyyy MM dd", CultureInfo.InvariantCulture);
+
+            var difference = 0;
+            if (firstDate >= secondDate)
+            {
+                difference = (firstDate - secondDate).Days;
+            }
+            else
+            {
+                difference = (secondDate - firstDate).Days;
+            }
+
+            return difference;
+        }
+    }
+}
---
+++ C#_OOP_Basics/Defining_Classes/Exercise/Defining Classes - Exercise/Date Modifier/StartUp.cs
@@ -0,0 +1,20 @@
+﻿/*
+Create a class DateModifier which stores the difference of the days between two Dates.It should have a method which 
+takes two string parameters representing a date as strings and calculates the difference in the days between them. 
+*/
+
+using System;
+
+namespace Date_Modifier
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var firstInputDate = Console.ReadLine();
+            var secondInputDate = Console.ReadLine();
+
+            Console.WriteLine(DateModifier.DifferenceBetweenTwoDates(firstInputDate, secondInputDate));
+        }
+    }
+}
