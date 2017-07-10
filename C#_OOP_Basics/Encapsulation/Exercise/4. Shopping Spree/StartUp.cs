a38fcb60f37723c1aed1dffcbee68e9bc9f9935b
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-07-10 21:08:53 +0300

    Shopping Spree Start Up information

--- C#_OOP_Basics/Encapsulation/Exercise/4. Shopping Spree/StartUp.cs
+++ C#_OOP_Basics/Encapsulation/Exercise/4. Shopping Spree/StartUp.cs
@@ -1,4 +1,33 @@
-﻿using System;
+﻿/*
+Create two classes: class Person and class Product. Each person should have a name, money and a bag of products. 
+Each product should have name and cost. Name cannot be an empty string. Money cannot be a negative number. Create 
+a program in which each command corresponds to a person buying a product. If the person can afford a product add 
+it to his bag. If a person doesn’t have enough money, print an appropriate message ("[Person name] can't afford 
+[Product name]"). On the first two lines you are given all people and all products. After all purchases print every 
+person in the order of appearance and all products that he has bought also in order of appearance. If nothing is bought
+, print the name of the person followed by "Nothing bought". In case of invalid input (negative money exception message: 
+"Money cannot be negative") or empty name (empty name exception message: "Name cannot be empty") break the program with 
+an appropriate message. See the examples below:
+
+Example input:
+Pesho=11;Gosho=4
+Bread=10;Milk=2;
+Pesho Bread
+Gosho Milk
+Gosho Milk
+Pesho Milk
+END
+
+output:
+Pesho bought Bread
+Gosho bought Milk
+Gosho bought Milk
+Pesho can't afford Milk
+Pesho - Bread
+Gosho - Milk, Milk
+*/
+
+using System;
 using System.Collections.Generic;
 
 class StartUp
