ebb1eb4a6456070fb32a8066ae352018414e0cd0
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-07-22 22:08:12 +0300

    Telephony exercise

---
+++ C#_OOP_Advanced/Interfaces and Abstraction/Exercises/Telephony/Smartphone.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+public class Smartphone : ICall, IBrowse
+{
+    public string Browse(string urlAddress)
+    {
+        return ValidUrlAddress(urlAddress)
+            ? "Invalid URL!"
+            : $"Browsing: {urlAddress}!";
+    }
+
+    public string Call(string callNumber)
+    {
+        return ValidCallNumber(callNumber)
+            ? $"Calling... {callNumber}"
+            : "Invalid number!";
+    }
+
+    private bool ValidUrlAddress(string urlAddress)
+    {
+        bool containsDigit = urlAddress.Any(char.IsDigit);
+        return containsDigit;
+    }
+
+    private bool ValidCallNumber(string callNumber)
+    {
+        bool containsOnlyDigit = callNumber.All(char.IsDigit);
+        return containsOnlyDigit;
+    }
+}
+
---
+++ C#_OOP_Advanced/Interfaces and Abstraction/Exercises/Telephony/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        var callNumbers = Console.ReadLine().Split(' ');
+        var urlAddresses = Console.ReadLine().Split(' ');
+
+        Smartphone phone = new Smartphone();
+
+        foreach (var number in callNumbers)
+        {
+            Console.WriteLine(phone.Call(number));
+        }
+
+        foreach (var url in urlAddresses)
+        {
+            Console.WriteLine(phone.Browse(url));
+        }
+    }
+}
+
