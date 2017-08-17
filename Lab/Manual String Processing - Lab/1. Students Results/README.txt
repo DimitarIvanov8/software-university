d9a778d12d9a66aa734eac5641a3c7a643fffac3
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-08-17 18:18:00 +0300

    Adding README for StudentsResults exercise

---
+++ Lab/Manual String Processing - Lab/1. Students Results/README.txt
@@ -0,0 +1,17 @@
+Write a program that reads number N from the console.After that read N lines
+with students with their results in format { name} - {firstResult}, 
+ {secondResult}, {thirdResult}
+Print table on console.Each row must contain: 
+•	CAdv - first result, align right with precision of 2
+•	COOP - second result, align right with precision of 2
+•	AdvOOP - third result, align right with precision of 2
+•	Average – average result with precision of 4
+•	Columns must be separated with "|"
+•	Don't forget heading row 
+
+Example input: 
+1
+Gosho - 3.33333, 4.4444, 5.555
+
+Output:
+Gosho     |   3.33|   4.44|   5.56| 4.4442|
--- Lab/Manual String Processing - Lab/1. Students Results/StudentsResults.cs
+++ Lab/Manual String Processing - Lab/1. Students Results/StudentsResults.cs
@@ -6,22 +6,6 @@ using System.Threading.Tasks;
 
 namespace _1.Students_Results
 {
-/*
-Write a program that reads number N from the console.After that read N lines
-with students with their results in format { name} - {firstResult}, 
- {secondResult}, {thirdResult}
-Print table on console.Each row must contain: 
-•	CAdv - first result, align right with precision of 2
-•	COOP - second result, align right with precision of 2
-•	AdvOOP - third result, align right with precision of 2
-•	Average – average result with precision of 4
-•	Columns must be separated with "|"
-•	Don't forget heading row 
-
-Example input: 
-1
-Gosho - 3.33333, 4.4444, 5.555
-*/
     class StudentsResults
     {
         static void Main(string[] args)
