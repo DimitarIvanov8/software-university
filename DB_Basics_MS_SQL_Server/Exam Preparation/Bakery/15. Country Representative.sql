c7d00f393cec7779bd0b86d5a86c29aef013198f
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-10-21 12:23:42 +0300

    Different solution to Exercise 15

--- DB_Basics_MS_SQL_Server/Exam Preparation/Bakery/15. Country Representative.sql
+++ DB_Basics_MS_SQL_Server/Exam Preparation/Bakery/15. Country Representative.sql
@@ -1,14 +1,14 @@
--- all countries with their most active distributor 
--- (the one with the greatest number of ingredients). 
--- If there are several distributors with most ingredients delivered, list them all. 
+SELECT CountryName, DistributorName
+FROM (
+  SELECT 
+    co.Name AS CountryName, d.Name AS DistributorName, 
+    COUNT(i.Id) AS IngredientsCount,
+    DENSE_RANK() OVER (PARTITION BY co.Name ORDER BY COUNT(i.Id) DESC) AS DistributorRank 
+  FROM Countries AS co
+  JOIN Distributors AS d ON d.CountryId = co.Id
+  JOIN Ingredients AS i ON i.DistributorId = d.Id
+  GROUP BY d.Name, co.Name
+) AS CountryDistributorStats
+WHERE DistributorRank = 1
+ORDER BY CountryName, DistributorName
 
-SELECT * FROM Countries
-SELECT * FROM Distributors
-SELECT * FROM Ingredients
-
-SELECT c.Name AS CountryName, 
-	   d.Name AS DisributorName 
-  FROM Countries AS c
-  JOIN Distributors AS d ON c.Id = d.CountryId
-  JOIN Ingredients AS i ON d.Id = i.DistributorId
-ORDER BY c.Name, d.Name
