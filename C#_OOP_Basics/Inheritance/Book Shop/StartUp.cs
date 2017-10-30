72d3f3b72e4778be6b38c8bb32dfd45ff3bdb00b
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-10-30 12:50:35 +0200

    Adding README.txt for Book Shop

--- C#_OOP_Basics/Inheritance/Book Shop/StartUp.cs
+++ C#_OOP_Basics/Inheritance/Book Shop/StartUp.cs
@@ -1,28 +1,4 @@
-﻿/*
-You are working in a library.And you are pissed of writing descriptions for books by hand, so you wish to use the computer 
-to speed up the process. The task is simple - your program should have two classes – one for the ordinary books – Book, and 
-another for the special ones – GoldenEditionBook.So let’s get started! We need two classes:
-•	Book - represents a book that holds title, author and price.A book should offer information about itself in the format shown in the output below.
-•	GoldenEditionBook - represents a special book holds the same properties as any Book, but its price is always 30% higher.
-
-Example input:
-Steven King
-Under the Dome
-22.20
-
-Output:
-Type: Book
-Title: Under the Dome
-Author: Steven King
-Price: 22.2
-
-Type: GoldenEditionBook
-Title: Under the Dome
-Author: Steven King
-Price: 28.9
-*/
-
-using System;
+﻿using System;
 
 namespace Book_Shop
 {
