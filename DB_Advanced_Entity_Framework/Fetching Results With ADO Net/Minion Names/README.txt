52245e308dbeee94f9290011f123d8059ea467b2
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-11-05 11:53:36 +0200

    Minion Names.csproj

---
+++ DB_Advanced_Entity_Framework/Fetching Results With ADO Net/Minion Names/README.txt
@@ -0,0 +1,29 @@
+Minion Names
+
+Write a program that prints on the console all minion names and age for a given villain id, ordered by name alphabetically.
+
+If there is no villain with the given ID, print "No villain with ID <VillainId> exists in the database.".
+If the selected villain has no minions, print "(no minions)" on the second row.
+Examples
+Input:
+1
+
+Output:
+Villain: Gru
+1. Bob 13
+2. Kevin 14
+3. Steward 19
+
+Input:
+3
+
+Output:
+Villain: Victor
+1. Bob 13
+2. Simon 22
+
+Input:
+10
+
+Output:
+No villain with ID 10 exists in the database.
---
+++ DB_Advanced_Entity_Framework/Fetching Results With ADO Net/Minion Names/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Minion_Names
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Server=DESKTOP-1SIRA4D\SQLEXPRESS;" +
+                "Database=MinionsDB;Integrated Security=True";
+            var connection = new SqlConnection(connectionString);
+
+            int villainId = int.Parse(Console.ReadLine());
+
+            connection.Open();
+            using (connection)
+            {
+                string villainQuery = "SELECT [Name] FROM Villains WHERE Id = @villainId";
+                var villainCommand = new SqlCommand(villainQuery, connection);
+                villainCommand.Parameters.AddWithValue("@villainId", villainId);
+
+                var reader = villainCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine($"Villain: {reader[0]}");
+                }
+                reader.Close();
+
+                string minionsQuery = "SELECT m.[Name], m.Age FROM Minions AS m " +
+                    "JOIN MinionsVillains AS mv ON m.Id = mv.MinionId " +
+                    "WHERE mv.VillainId = @villainId";
+                var minionCommand = new SqlCommand(minionsQuery, connection);
+                minionCommand.Parameters.AddWithValue("@villainId", villainId);
+                reader = minionCommand.ExecuteReader();
+                int counter = 1;
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{counter}. {reader[0]} {reader[1]}");
+                    counter++;
+                }
+            }
+        }
+    }
+}
