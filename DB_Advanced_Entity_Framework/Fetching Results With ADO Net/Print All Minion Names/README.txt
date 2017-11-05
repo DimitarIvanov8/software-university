3951d3ac93fdc2ec1c9489743b0bf11fdfae0a6f
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-11-05 16:23:37 +0200

    Print All Minion Names exercise

---
+++ DB_Advanced_Entity_Framework/Fetching Results With ADO Net/Print All Minion Names/README.txt
@@ -0,0 +1,22 @@
+Print All Minion Names
+
+Write a program that prints all minion names from the minions table in the following order: first record, last record, first + 1, last - 1, first + 2, last - 2 … first + n, last - n. 
+[1, 10, 2, 9, 3, 8, 4, 7, 5, 6]
+
+Original Order:
+Bob
+Kevin
+Steward
+Jimmy
+Vicky
+Becky
+Jully
+
+Output:
+Bob
+Jully
+Kevin
+Becky
+Steward
+Vicky
+Jimmy
---
+++ DB_Advanced_Entity_Framework/Fetching Results With ADO Net/Print All Minion Names/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Print_All_Minion_Names
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Server=DESKTOP-1SIRA4D\SQLEXPRESS;" +
+                "Database=MinionsDB;Integrated Security=True";
+            var connection = new SqlConnection(connectionString);
+
+            connection.Open();
+            using (connection)
+            {
+                try
+                {
+                    var command = new SqlCommand("SELECT Name FROM Minions", connection);
+                    var reader = command.ExecuteReader();
+                    var names = new List<string>();
+                    using (reader)
+                    {
+                        try
+                        {
+                            while (reader.Read())
+                            {
+                                names.Add(Convert.ToString(reader[0]));
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                    }
+
+                    for (int i = 0; i < names.Count / 2; i++)
+                    {
+                        Console.WriteLine(names[i]);
+                        Console.WriteLine(names[names.Count - 1 - i]);
+                    }
+                    if (names.Count % 2 != 0)
+                    {
+                        Console.WriteLine(names[names.Count / 2]);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
