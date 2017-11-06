ca06f7b9f8addc1a07667e75b94f49cedca77a2a
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-11-06 16:57:39 +0200

    Introduction to Entity Framework - homework

---
+++ DB_Advanced_Entity_Framework/Introduction to Entity Framework/P02_DatabaseFirst/README.txt
@@ -0,0 +1,9 @@
+Employees Full Information
+Now we can use the SoftUniContext to extract data from our database. Your first task is to extract all employees and print their first, last and middle name, their job title and salary, rounded to 2 symbols after the decimal separator, all of those separated with a space. Order them by employee id.
+
+Output:
+Guy Gilbert R Production Technician 12500.00
+Kevin Brown F Marketing Assistant 13500.00
+Roberto Tamburello  Engineering Manager 43300.00
+Rob Walters  Senior Tool Designer 29800.00
+…
---
+++ DB_Advanced_Entity_Framework/Introduction to Entity Framework/P02_DatabaseFirst/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using P02_DatabaseFirst.Data;
+using System.Linq;
+
+namespace P02_DatabaseFirst
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            var employees = context
+                .Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary,
+                    e.EmployeeId
+                })
+                .OrderBy(e => e.EmployeeId)
+                .ToList();
+
+            foreach (var e in employees)
+            {
+                Console.WriteLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+            }
+        }
+    }
+}
