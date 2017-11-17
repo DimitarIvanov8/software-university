821dadaf3f179207a4c2110fe3a8baae263697c3
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-11-17 23:31:27 +0200

    P03_FootballBetting database

---
+++ DB_Advanced_Entity_Framework/Entity Relations/P03_FootballBetting/P03_FootballBetting.App/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using P03_FootballBetting.Data;
+using P03_FootballBetting.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace P03_FootballBetting.App
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new FootballBettingContext())
+            {
+                db.Database.EnsureCreated();
+            }
+        }
+    }
+}
