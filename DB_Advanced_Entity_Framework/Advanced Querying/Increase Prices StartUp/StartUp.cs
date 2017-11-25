a3b3d8c7a3b46e7eaa10c2dc129535614bb7a130
DimitarIvanov8 <dimitar.ivanov300@gmail.com>
2017-11-25 16:20:38 +0200

    Last exercises from Advanced Querying homework

---
+++ DB_Advanced_Entity_Framework/Advanced Querying/Increase Prices StartUp/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace BookShop
+{
+    using BookShop.Data;
+    using BookShop.Models;
+    using BookShop.Initializer;
+    using System.Linq;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            using (var db = new BookShopContext())
+            {
+                IncreasePrices(db);
+            }
+        }
+
+        public static void IncreasePrices(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.ReleaseDate.Value.Year < 2010)
+                .ToArray();
+                
+            foreach (var book in books)
+            {
+                book.Price += 5m;
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ DB_Advanced_Entity_Framework/Advanced Querying/Remove Books StartUp/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace BookShop
+{
+    using BookShop.Data;
+    using BookShop.Models;
+    using BookShop.Initializer;
+    using System.Linq;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            using (var db = new BookShopContext())
+            {
+                Console.WriteLine($"{RemoveBooks(db)} books were deleted");
+            }
+        }
+
+        public static int RemoveBooks(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.Copies < 4200)
+                .ToArray();
+
+            int result = books.Count();
+            
+			context.RemoveRange(books);
+			
+            context.SaveChanges();
+
+            return result;
+        }
+    }
+}
